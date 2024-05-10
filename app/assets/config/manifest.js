2557e6528c8936b377eb4b99cc6c55edca7a3981
Oleksandr Shynkarenko <57036990+AndrasteCries@users.noreply.github.com>
2024-05-10 14:04:05 +0300

    Add main page(application controller index def), Add root route application#index, Add some styles scss

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link_directory ../stylesheets/layout/ .scss
+//= link layout/content.scss
+//= link layout/header.scss
