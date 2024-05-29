89473ad4ee083913b371d11cd84bb7013c2f3b66
Oleksandr Shynkarenko <57036990+AndrasteCries@users.noreply.github.com>
2024-05-29 14:28:14 +0300

    Add local languages engl and ukrainian, anime pages and header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  before_action :redirect_to_locale
   def index
+    @def_local = I18n.locale
     render "pages/main"
+  end
+
+  private
+
+  def redirect_to_locale
+    if request.path == "/" && !params[:locale].present?
+      redirect_to root_path(locale: "en")
+    end
+  end
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+  def default_url_options
+    {locale: I18n.locale}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,25 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root "application#index"
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :users, path: "users", controllers: {
-    session: "users/sessions",
-    registrations: "users/registrations"
-  }
-  resources :topics do
-    resources :topic_comments, only: :create
+  scope "/:locale" do
+    devise_for :admin_users, ActiveAdmin::Devise.config
+    devise_for :users, path: "users", controllers: {
+      session: "users/sessions",
+      registrations: "users/registrations"
+    }
+    resources :topics do
+      resources :topic_comments, only: :create
+    end
+    resources :forums
+    resources :forum_tags
+    resources :characters
+    resources :animes do
+      resources :comments, only: :create
+    end
+    resources :user_rates
+    resources :user
+    get "users/:id/", to: "user#show", as: :user_profile
+    get "users/:id/edit", to: "user#edit", as: :edit_user_profile
+    get "up" => "rails/health#show", as: :rails_health_check
   end
-  resources :forums
-  resources :forum_tags
-  resources :characters
-  resources :animes do
-    resources :comments, only: :create
-  end
-  resources :user_rates
-  resources :user
-  get "users/:id/", to: "user#show", as: :user_profile
-  get "users/:id/edit", to: "user#edit", as: :edit_user_profile
-  get "up" => "rails/health#show", as: :rails_health_check
 end
