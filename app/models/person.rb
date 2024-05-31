25f4573b71e46f794fa92660b5717e96c5fa494b
Pavlo Zimnytsia <paha20zimnica16@gmail.com>
2024-05-31 14:15:05 +0300

    Update Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -16,6 +16,6 @@ class Person < ApplicationRecord
   has_one_attached :image
 
   def self.ransackable_attributes(auth_object = nil)
-    ["id", "name", "japanese", "image", "website", "spec", "birth", "created_at", "updated_at"]
+    ["id", "name", "japanese", "website", "birthday", "spec", "studio_id", "created_at", "updated_at"]
   end
 end
