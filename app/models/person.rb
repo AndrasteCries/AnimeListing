1c78bd3fbc8ab545a253abd6c94261fd837a64b5
Pavlo Zimnytsia <paha20zimnica16@gmail.com>
2024-05-31 05:31:29 +0300

    Add 'Person' content

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,21 @@
 class Person < ApplicationRecord
 
+  has_many :anime_characters
+  has_many :animes, through: :anime_characters
+  has_many :comments, dependent: :destroy
+
+  enum spec: {
+
+  }
+
+  validates :name, presence: true
+  validates :spec, presence: true
+  validates :birth, presence: true
+  validates :image, presence: true
+
+  has_one_attached :image
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "name", "japanese", "image", "website", "spec", "birth", "created_at", "updated_at"]
+  end
 end
