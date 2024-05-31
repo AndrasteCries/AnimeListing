4747e839e39b38c32eaec0de2bf1971eb76c9e18
Pavlo Zimnytsia <paha20zimnica16@gmail.com>
2024-05-31 05:19:02 +0300

    Add 'Review' content

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,15 @@
 class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :anime
 
+  validates :text, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true,
+                                                     greater_than_or_equal_to: 1,
+                                                     less_than_or_equal_to: 10 }
+
+  has_one_attached :image
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "user_id", "body", "opinion", "before_release", "comments_count", "created_at", "updated_at"]
+  end
 end
